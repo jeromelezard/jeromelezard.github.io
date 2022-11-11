28fd0baeb34884d9ec78ea1e8fd5f77112548238
jeromelezard <jerome.lezard@gmail.com>
2022-11-11 14:33:54 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const banner = document.getElementById("top");
+const topBanner = document.getElementById("topNav");
+const icons = document.getElementById("icons");
+const header = document.getElementById("header");
+const subHeader = document.getElementById("subHeader");
+const arrow = document.getElementById("arrow");
+const navTitle = document.getElementById("navTitle");
+const navIcons = document.getElementById("navIcons");
+
+HTMLElement.prototype.alpha = function (a) {
+    current_color = getComputedStyle(this).getPropertyValue("background-color");
+    match = /rgba?\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*(,\s*\d+[\.\d+]*)*\)/g.exec(
+        current_color
+    );
+    a = a > 1 ? a / 100 : a;
+    this.style.backgroundColor =
+        "rgba(" + [match[1], match[2], match[3], a].join(",") + ")";
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    navTitle.classList.remove("hide");
+    navTitle.classList.remove("hide");
+});
+document.body.addEventListener("touchmove", onScroll);
+document.addEventListener("scroll", onScroll);
+function onScroll() {
+    console.log(window.scrollY);
+    // console.log(subHeader.getBoundingClientRect().top);
+    // if (banner.clientHeight - window.scrollY <= 90) {
+    //     topBanner.classList.add("sticky");
+    // } else {
+    //     topBanner.classList.remove("sticky");
+    // }
+    if (window.scrollY >= 450 && window.scrollY <= 1050) {
+        console.log(window.scrollY);
+        // topBanner.style.opacity = (-2/15) * window.scrollY + 160;
+        topBanner.alpha((-2 / 15) * window.scrollY + 160);
+        navTitle.style.opacity = 1;
+    } else if (window.scrollY > 1050) {
+        // topBanner.style.opacity = 0.7;
+        topBanner.alpha(20);
+    } else if (window.scrollY < 450) {
+        topBanner.alpha(100);
+    }
+    if (icons.getBoundingClientRect().top <= 65) {
+        navTitle.classList.remove("hide-title");
+        navIcons.classList.remove("hide-icons");
+    } else {
+        navTitle.classList.add("hide-title");
+        navIcons.classList.add("hide-icons");
+    }
+}
+
+arrow.addEventListener("click", () => {
+    document.getElementById("location").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("emailBody").addEventListener("click", () => {
+    let email = "jerome.lezard@gmail.com";
+    navigator.clipboard.writeText(email);
+
+    document.getElementById("copiedBody").innerHTML = "Email copied!";
+});
+document.getElementById("emailNav").addEventListener("click", () => {
+    let email = "jerome.lezard@gmail.com";
+    navigator.clipboard.writeText(email);
+
+    document.getElementById("copiedNav").innerHTML = "Email copied!";
+});
+document.getElementById("icons").addEventListener("mouseleave", () => {
+    document.getElementById("copiedBody").innerHTML = "Click to copy email";
+});
+document.getElementById("navIcons").addEventListener("mouseleave", () => {
+    document.getElementById("copiedNav").innerHTML = "Click to copy email";
+});
